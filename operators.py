dcf727381436a16a67fcec100c787caa3912c5a6
unknown <rapoluindhu2929@gmail.com>
2024-12-19 22:35:29 +0530

    Python operators

---
+++ operators.py
@@ -0,0 +1,86 @@
+# -------------------------------------------operators----------------------------
+# 1.Arithmetic operators
+# 2.Assignment operators
+# 3.Comparison operators
+# 4.Logical operators
+# 5.Identity operators
+# 6.Membership operators
+# 7.Bitwise operators
+
+# ---------------------1.Arithmetic operators---------------
+print("Arithmetic operators")
+x=30
+y=20
+print("x=",x)
+print("y=",y)
+print("x+y=",x+y)
+print("x-y=",x-y)
+print("x*y=",x*y)
+print("x**y=",x**y)
+print("x/y=",x/y)
+print("x//y=",x//y)
+print()
+# ---------------------2.Assignment operators-----------------
+print("Assignment operators")
+num=100
+print("Assigning 100 to num =",num)
+num//=2
+print("num//=2 =",num)
+num*=2
+print("num*=2 =",num)
+num**=2
+print("num**=2 =",num)
+num-=100
+print("num-=100 =",num)
+num/=2
+print("num/=2 =",num)
+num%=2
+print("num%=2 =",num)
+print()
+
+# ----------------------------3.Comparison operators---------------------
+print("Comparison operators / Relational operator")
+i=22
+j=44
+print("i =",i)
+print("j =",j)
+print("i==j =", i==j)
+print("i!=j =", i!=j)
+print("i>j =",i>j)
+print("i<j =",i<j)
+print("i>=j =",i>=j)
+print("i<=j =",i<=j)
+print()
+
+# -----------------------------4.Logical operators------------------------
+print("Logical operators")
+a=20
+b=50
+c=True
+print("a =",a)
+print("b =",b)
+print("c=",c)
+print("a>b and a<b =",a>b and a<b)
+print("a>b or a<b =",a>b or a<b)
+print("not c =",not c)
+print()
+
+
+# --------------------------5.Identity Operators----------------------------
+print("Identity Operators")
+# is:--Returns True if both variables are the same object
+# is no:--Returns True if both variables are not the same object
+
+print()
+
+# ---------------------------6.MeMembership Operators--------------------------
+print("Membership Operators")
+print()
+# ---------------------------7.Bitwise Operators--------------------------
+print("Bitwise Operators")
+print()
+
+# --------------------------------------------------------
+print(2 and -2)
+print(5 and 2)
+print()
